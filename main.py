40ab36967497137de2aa0d573f65131da49ccc0f
ben <fourty400@gmail.com>
2019-11-16 21:00:09 +0300

    Remove logout popup modal

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request, url_for, redirect, session
 import pymysql
 import re
 app = Flask(__name__)
 
-
-# connection
-app.secret_key = '4539Clinic201kfinjdsk'
 
 # Enter your database connection details below
 # Enter your database connection details below
@@ -18,18 +14,15 @@ mysql = pymysql.connect(host='localhost',
                              cursorclass=pymysql.cursors.DictCursor)
 
 # Intialize MySQL
-=======
-from flask import Flask,render_template,url_for,request,redirect,session
-app=Flask(__name__)
+app = Flask(__name__)
 app.secret_key = 'super secret key'
->>>>>>> 48a25fc78f4785768c8969b6aab5910495db69bc
 
 
 @app.route('/')
 def home():
-    if 'loggedin' in session:
+    if 'user' in session:
         # User is loggedin show them the home page
-        return render_template('index.html', username=session['username'])
+        return render_template('index.html', username=session['user']['username'])
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
@@ -41,21 +34,20 @@ def user():
 
 @app.route('/logout')
 def logout():
-    session.pop('user',None)
+    session.pop('user', None)
     return redirect(url_for('login'))
 
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-     if request.method == 'POST':
-         user=request.form['email']
-         password=request.form['password']
-         session['user']=request.form
-         #session.commit()
-         return redirect(url_for('user'))
-     else:
-        return render_template('login.html')
-
+    if request.method == 'POST':
+        # user = request.form['email']
+        # password = request.form['password']
+        session['user'] = request.form
+        # session.commit()
+        return redirect(url_for('home'))
+    else:
+        return render_template('auth/login.html')
 
 
 if(__name__) == '__main__':
